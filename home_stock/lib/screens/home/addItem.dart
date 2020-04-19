a800d59e0f6fdab836f83a1ac5fa4c49983a1fb3
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-04-19 21:32:25 +0530

    System Padding for andd and edit panels

--- home_stock/lib/screens/home/addItem.dart
+++ home_stock/lib/screens/home/addItem.dart
@@ -1,6 +1,7 @@
 // import 'package:employee_activity_tracker/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:home_stock/models/user.dart';
+import 'package:home_stock/screens/shared/systemPadding.dart';
 import 'package:home_stock/services/database.dart';
 import 'package:provider/provider.dart';
 
@@ -31,63 +32,66 @@ class _AddItemState extends State<AddItem> {
     _category = _category ?? categories[0];
     _metric = _metric ?? metrics[0];
 
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Add Item',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Item Name'),
-            validator: (val) => val.isEmpty ? 'Please enter a Name' : null,
-            onChanged: (val) => setState(() => _name = val),
-          ),
-          SizedBox(height: 20.0),
-          DropdownButtonFormField(
-            value: _category ?? categories[0],
-            items: categories.map((item){
-              return DropdownMenuItem(
-                value: item,
-                child: Text('$item'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _category = val),
-          ),
-          DropdownButtonFormField(
-            value: _metric ?? metrics[0],
-            items: metrics.map((item){
-              return DropdownMenuItem(
-                value: item,
-                child: Text('$item'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _metric = val),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Quantity'),
-            keyboardType: TextInputType.number,
-            validator: (val) => val.isEmpty ? 'Please enter Quantity' : null,
-            onChanged: (val) => setState(() => _quantity = int.parse(val)),
-          ),
-          SizedBox(height: 20.0),
-          RaisedButton(
-            color: Colors.blue,
-            child: Text(
+
+    return new SystemPadding(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Text(
               'Add Item',
-              style: TextStyle(color: Colors.white)
+              style: TextStyle(fontSize: 18.0),
             ),
-            onPressed: () async {
-              if(_formKey.currentState.validate()){
-                await DatabaseService(uid: listForUser.uid).updateItemData(_name, _category, _quantity, _metric, 0);
-                Navigator.pop(context);
-              }
-            },
-          ),
-        ],
+            SizedBox(height: 20.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Item Name'),
+              validator: (val) => val.isEmpty ? 'Please enter a Name' : null,
+              onChanged: (val) => setState(() => _name = val),
+            ),
+            SizedBox(height: 20.0),
+            DropdownButtonFormField(
+              value: _category ?? categories[0],
+              items: categories.map((item){
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text('$item'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _category = val),
+            ),
+            DropdownButtonFormField(
+              value: _metric ?? metrics[0],
+              items: metrics.map((item){
+                return DropdownMenuItem(
+                  value: item,
+                  child: Text('$item'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _metric = val),
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Quantity'),
+              keyboardType: TextInputType.number,
+              validator: (val) => val.isEmpty ? 'Please enter Quantity' : null,
+              onChanged: (val) => setState(() => _quantity = int.parse(val)),
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                'Add Item',
+                style: TextStyle(color: Colors.white)
+              ),
+              onPressed: () async {
+                if(_formKey.currentState.validate()){
+                  await DatabaseService(uid: listForUser.uid).updateItemData(_name, _category, _quantity, _metric, 0);
+                  Navigator.pop(context);
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- home_stock/lib/screens/home/editItem.dart
+++ home_stock/lib/screens/home/editItem.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:home_stock/models/item.dart';
 import 'package:home_stock/models/user.dart';
+import 'package:home_stock/screens/shared/systemPadding.dart';
 import 'package:home_stock/services/database.dart';
 import 'package:provider/provider.dart';
 
@@ -37,64 +38,66 @@ class _EditItemState extends State<EditItem> {
     _metric = _metric ?? widget.item.metric;
     _quantity = _quantity ?? widget.item.quantity;
 
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          Text(
-            'Edit Item',
-            style: TextStyle(fontSize: 18.0),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            initialValue: _name ?? 'Name',
-            validator: (val) => val.isEmpty ? 'Please enter a Name' : null,
-            onChanged: (val) => setState(() => _name = val),
-          ),
-          SizedBox(height: 20.0),
-          DropdownButtonFormField(
-            value: _category.isEmpty ? null : categories[categories.indexOf(_category)],
-            items: categories.map((category){
-              return DropdownMenuItem(
-                value: category,
-                child: Text('$category'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _category = val),
-          ),
-          DropdownButtonFormField(
-            value: _metric.isEmpty ? null : metrics[metrics.indexOf(_metric)],
-            items: metrics.map((metric){
-              return DropdownMenuItem(
-                value: metric,
-                child: Text('$metric'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _metric = val),
-          ),
-          SizedBox(height: 20.0),
-          TextFormField(
-            initialValue: _quantity.toString().isEmpty ? '0' : _quantity.toString(),
-            keyboardType: TextInputType.number,
-            validator: (val) => val.isEmpty ? 'Please enter Quantity' : null,
-            onChanged: (val) => setState(() => _quantity = int.parse(val)),
-          ),
-          SizedBox(height: 20.0),
-          RaisedButton(
-            color: Colors.blue,
-            child: Text(
+    return new SystemPadding(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Text(
               'Edit Item',
-              style: TextStyle(color: Colors.white)
+              style: TextStyle(fontSize: 18.0),
             ),
-            onPressed: () async {
-              if(_formKey.currentState.validate()){
-                await DatabaseService(uid: listForUser.uid).updateItemData(_name, _category, _quantity, _metric, widget.item.inShoppingList);
-                Navigator.pop(context);
-              }
-              // Navigator.pop(context);
-            },
-          ),
-        ],
+            SizedBox(height: 20.0),
+            TextFormField(
+              initialValue: _name ?? 'Name',
+              validator: (val) => val.isEmpty ? 'Please enter a Name' : null,
+              onChanged: (val) => setState(() => _name = val),
+            ),
+            SizedBox(height: 20.0),
+            DropdownButtonFormField(
+              value: _category.isEmpty ? null : categories[categories.indexOf(_category)],
+              items: categories.map((category){
+                return DropdownMenuItem(
+                  value: category,
+                  child: Text('$category'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _category = val),
+            ),
+            DropdownButtonFormField(
+              value: _metric.isEmpty ? null : metrics[metrics.indexOf(_metric)],
+              items: metrics.map((metric){
+                return DropdownMenuItem(
+                  value: metric,
+                  child: Text('$metric'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _metric = val),
+            ),
+            SizedBox(height: 20.0),
+            TextFormField(
+              initialValue: _quantity.toString().isEmpty ? '0' : _quantity.toString(),
+              keyboardType: TextInputType.number,
+              validator: (val) => val.isEmpty ? 'Please enter Quantity' : null,
+              onChanged: (val) => setState(() => _quantity = int.parse(val)),
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                'Edit Item',
+                style: TextStyle(color: Colors.white)
+              ),
+              onPressed: () async {
+                if(_formKey.currentState.validate()){
+                  await DatabaseService(uid: listForUser.uid).updateItemData(_name, _category, _quantity, _metric, widget.item.inShoppingList);
+                  Navigator.pop(context);
+                }
+                // Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
