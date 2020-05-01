c073c313e73bc8e0d024f92f3a55be0e3d689bee
Tharinda Dilshan Piyadasa <tharindad7@gmail.com>
2020-05-01 13:34:01 +0530

    Bugs Fixed

--- home_stock/lib/screens/notifications/notifications.dart
+++ home_stock/lib/screens/notifications/notifications.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:home_stock/models/item.dart';
 import 'package:home_stock/models/notification.dart';
 import 'package:home_stock/models/user.dart';
 import 'package:home_stock/screens/home/itemTile.dart';
+import 'package:home_stock/screens/settings/shareInventory.dart';
 import 'package:home_stock/services/database.dart';
 import 'package:provider/provider.dart';
 
@@ -15,11 +17,15 @@ class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+    final listForUser = Provider.of<UserData>(context);
+
     //final listForUser = Provider.of<UserData>(context);
 
     //List<Item> items = Provider.of<List<Item>>(context) ?? [];
     List<FirestoreNotification>notifications = [
-      FirestoreNotification(timestamp: 'timestamp', title: 'title', message: 'message')
+      FirestoreNotification(timestamp: 'timestamp', title: 'title', message: 'message'),
+      FirestoreNotification(timestamp: 'timestamp', title: 'Share Request', message: 'message')
     ];
 
 
@@ -38,8 +44,20 @@ class _NotificationsState extends State<Notifications> {
                   return Card(
                     child: ListTile(
                       title: Text(notifications[index].title),
-                      subtitle: Text(notifications[index].message),
-                      onTap: () {print('notification clicked');},
+                      subtitle: notifications[index].title == 'Share Request' ? Text(notifications[index].message) : Text('Tis a request'),
+                      onTap: notifications[index].title == 'Share Request' ? () async{
+                    Navigator.push(context, 
+                      MaterialPageRoute(builder: (context) => 
+                        StreamProvider<List<Item>>.value(
+                          value: DatabaseService(uid: listForUser.items).itemData,
+                          child: StreamProvider<UserData>.value(
+                            value: DatabaseService(uid: listForUser.email).userData,
+                            child: ShareInventory()
+                          )
+                        )
+                      ),
+                    );
+                  } : () {print('notification clicked');},
                     ),
                   );
                 },
